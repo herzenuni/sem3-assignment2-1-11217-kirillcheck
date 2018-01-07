0673d1117517340a4542c53ffe2ea8f0b9ea5a7e
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-08 01:35:35 +0300

    Create (shakhov)tests.py

---
+++ sorting/(shakhov)tests.py
@@ -0,0 +1,11 @@
+# Шахов Кирилл
+
+from sort import sort
+import pytest
+
+
+@pytest.mark.parametrize("a, result", [(range(10, 100), ['14', '28', '29', '35', '41',
+                                                         '53', '67', '76', '82', '92'])])
+
+def test_sort(a, result):
+    assert sort(a) == result
