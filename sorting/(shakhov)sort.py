f9cdb262d3fbd95213edc20ae8705cc57aa9705a
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-08 01:32:49 +0300

    Create (shakhov)sort.py

---
+++ sorting/(shakhov)sort.py
@@ -0,0 +1,21 @@
+# Практичское задание NOD
+# Реализуйте два алгоритма сортировки: методом пузырька и быструю сортировку, протестируйте их работу с использованием пакета hypothesis
+# Шахов Кирилл
+# ИВТ
+
+def sort(x):
+  list = []
+  for i in x:
+      i = str(i)
+      s = 0
+      for j in i:
+          s += int(j)**2
+      if 0 == s % 17:
+          list.append(i)
+  return list
+
+def main():
+    print(' Result: ', sort(range(10,100)))
+
+if __name__ == '__main__':
+    main()
